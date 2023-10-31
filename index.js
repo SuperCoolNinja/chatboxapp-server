fa521deb446e00fecbaa509cfbec5f717f475464
SuperCoolNinja <sbpro570@gmail.com>
2023-10-31 18:20:18 +0100

    cors

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const http = require("http");
 const helmet = require("helmet");
 const cors = require("cors");
 const { Server } = require("socket.io");
-const handle_events = require("./socket/events.js");
+const handle_events = require("./src/socket/events.js");
+
+require("dotenv").config();
 
 // Configuration :
 const app = express();
@@ -13,7 +15,12 @@ const Server_PORT = process.env.PORT || 4000;
 
 // Middlewares :
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    orgin: "*",
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+  })
+);
 
 // Setup Socket.io :
 const io = new Server(server, {
@@ -23,10 +30,12 @@ const io = new Server(server, {
   },
 });
 
-// handle all events : 
+app.get("/", (req, res) => res.send("Hello world!"));
+
+// handle all events :
 handle_events(io);
 
 // Setup serveur :
 server.listen(Server_PORT, () =>
-  console.log(`Server listening on port http://localhost:${Server_PORT}`)
+  console.log(`Server listening on port ${Server_PORT}`)
 );
